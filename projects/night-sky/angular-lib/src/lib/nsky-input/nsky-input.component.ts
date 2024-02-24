d09c5a76d0328a3266109f861df6432dc9640304
Jhonathan Peres <dotyomusic@gmail.com>
2024-02-23 21:08:34 -0300

    [UPDATE] Criação de componente input

---
+++ projects/night-sky/angular-lib/src/lib/nsky-input/nsky-input.component.ts
@@ -0,0 +1,39 @@
+import { NgClass } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'nsky-input',  // Corrija o seletor aqui
+  templateUrl: './nsky-input.component.html',
+  styleUrls: ['./nsky-input.component.css'],
+  standalone: true, // Make sure to include this line
+  imports: []
+})
+
+export class NskyInputComponent {
+  validationInput: string = '';
+  classCss: string = 'input-text';
+
+  @Input() text: string = '';
+  @Input() placeholder: string = '';
+  @Input() disabled: boolean = false;
+  @Input() id: string = '';
+  @Input() name: string = '';
+  @Input() type: string = '';
+  @Input() maxlength: string = '';
+  @Input() max: string = '';
+  @Input() min: string = '';
+  @Input('validation')
+  set validation(value: string) {
+    if(value === 'invalid') {
+      this.validationInput = 'invalid'
+      this.classCss = 'input-error'
+    } else if (value === 'valid') {
+      this.validationInput = 'valid'
+      this.classCss = 'input-text'
+    } else {
+      this.validationInput = ''
+      this.classCss = 'input-text'
+    }
+  }
+  @Input() messageError: string = '';
+}
--- projects/night-sky/angular-lib/src/public-api.ts
+++ projects/night-sky/angular-lib/src/public-api.ts
@@ -3,4 +3,5 @@
  */
 
 export * from './lib/nsky-button/nsky-button.component'
+export * from './lib/nsky-input/nsky-input.component'
 export * from './lib/night-sky.module'
